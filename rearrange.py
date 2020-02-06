59c76fd2f5e34cb2d021d922838bdfce498b793e
Dahveyea <dahveyea.cowan@gmail.com>
2020-02-05 22:28:28 -0800

    Added rearrange module

---
+++ rearrange.py
@@ -0,0 +1,22 @@
+import random
+
+
+
+
+s = '''The wind whistles past my ears.
+Closing my eyes, I lose all my fears.'''.split("\n")
+
+
+def lines_printed_random(s):
+
+    lines = []
+
+    for i in range(0, len(s)):
+        index = random.randint(0, len(s) - 1)
+        lines.append(s[index])
+
+    print("\n".join(lines))
+
+
+if __name__ == '__main__':
+    lines_printed_random(s)
